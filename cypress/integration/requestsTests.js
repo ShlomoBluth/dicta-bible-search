7e09217e98b14f441c64fe700f65d903da681c57
Shlomo Bluth <lomibluth@gmail.com>
2021-08-24 13:51:59 +0300

    add commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -17,86 +17,86 @@ sizes.forEach((size) => {
   
     
   
-    it('Error message for textAnalysis response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/textAnalysis',
-        language:'Hebrew',
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        delaySeconds:  60*5
-      })
-    })
+    // it('Error message for textAnalysis response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/textAnalysis',
+    //     language:'Hebrew',
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+    //     delaySeconds:  60*5
+    //   })
+    // })
   
-    it('Error message for textAnalysis response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in english mode',()=>{
-      cy.searchRequest({
-        url:'/textAnalysis',
-        language:'English',
-        message:'Oops. Something went wrong Please try again later',
-        delaySeconds: 60*5
-      })
-    })
+    // it('Error message for textAnalysis response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/textAnalysis',
+    //     language:'English',
+    //     message:'Oops. Something went wrong Please try again later',
+    //     delaySeconds: 60*5
+    //   })
+    // })
   
-    it('Error message for textAnalysis response with status code 500 when clicking the run button of search page'+
-    ' in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/textAnalysis',
-        language:'Hebrew',
-        status:500,
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-      })
-    })
+    // it('Error message for textAnalysis response with status code 500 when clicking the run button of search page'+
+    // ' in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/textAnalysis',
+    //     language:'Hebrew',
+    //     status:500,
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+    //   })
+    // })
   
-    it('Error message for textAnalysis response with status code 500 when clicking the run button of search page'+
-    ' in english mode',()=>{
-      cy.searchRequest({
-        url:'/textAnalysis',
-        language:'English',
-        status:500,
-        message:'Oops. Something went wrong Please try again later'
-      })
-    })
+    // it('Error message for textAnalysis response with status code 500 when clicking the run button of search page'+
+    // ' in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/textAnalysis',
+    //     language:'English',
+    //     status:500,
+    //     message:'Oops. Something went wrong Please try again later'
+    //   })
+    // })
     
     
-    it('Error message for related response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/related',
-        language:'Hebrew',
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        delaySeconds:  60*5
-      })
-    })
+    // it('Error message for related response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/related',
+    //     language:'Hebrew',
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+    //     delaySeconds:  60*5
+    //   })
+    // })
   
-    it('Error message for related response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in english mode',()=>{
-      cy.searchRequest({
-        url:'/related',
-        language:'English',
-        message:'Oops. Something went wrong Please try again later',
-        delaySeconds: 60*5
-      })
-    })
+    // it('Error message for related response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/related',
+    //     language:'English',
+    //     message:'Oops. Something went wrong Please try again later',
+    //     delaySeconds: 60*5
+    //   })
+    // })
   
-    it('Error message for related response with status code 500 when clicking the run button of search page'+
-    ' in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/related',
-        language:'Hebrew',
-        status:500,
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-      })
-    })
+    // it('Error message for related response with status code 500 when clicking the run button of search page'+
+    // ' in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/related',
+    //     language:'Hebrew',
+    //     status:500,
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+    //   })
+    // })
   
-    it('Error message for related response with status code 500 when clicking the run button of search page'+
-    ' in english mode',()=>{
-      cy.searchRequest({
-        url:'/related',
-        language:'English',
-        status:500,
-        message:'Oops. Something went wrong Please try again later'
-      })
-    })
+    // it('Error message for related response with status code 500 when clicking the run button of search page'+
+    // ' in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/related',
+    //     language:'English',
+    //     status:500,
+    //     message:'Oops. Something went wrong Please try again later'
+    //   })
+    // })
     
     
     it('Error message for search response with a delay of 5 minutes when clicking the run button'+
@@ -141,128 +141,128 @@ sizes.forEach((size) => {
   
   
   
-    it('Error message for books response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/books',
-        language:'Hebrew',
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        delaySeconds:  60*5
-      })
-    })
+    // it('Error message for books response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/books',
+    //     language:'Hebrew',
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+    //     delaySeconds:  60*5
+    //   })
+    // })
   
-    it('Error message for books response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in english mode',()=>{
-      cy.searchRequest({
-        url:'/books',
-        language:'English',
-        message:'Oops. Something went wrong Please try again later',
-        delaySeconds: 60*5
-      })
-    })
+    // it('Error message for books response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/books',
+    //     language:'English',
+    //     message:'Oops. Something went wrong Please try again later',
+    //     delaySeconds: 60*5
+    //   })
+    // })
   
-    it('Error message for books response with status code 500 when clicking the run button of search page'+
-    ' in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/books',
-        language:'Hebrew',
-        status:500,
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-      })
-    })
+    // it('Error message for books response with status code 500 when clicking the run button of search page'+
+    // ' in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/books',
+    //     language:'Hebrew',
+    //     status:500,
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+    //   })
+    // })
   
-    it('Error message for books response with status code 500 when clicking the run button of search page'+
-    ' in english mode',()=>{
-      cy.searchRequest({
-        url:'/books',
-        language:'English',
-        status:500,
-        message:'Oops. Something went wrong Please try again later'
-      })
-    })  
+    // it('Error message for books response with status code 500 when clicking the run button of search page'+
+    // ' in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/books',
+    //     language:'English',
+    //     status:500,
+    //     message:'Oops. Something went wrong Please try again later'
+    //   })
+    // })  
   
   
-    it('Error message for lexemes response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/lexemes',
-        language:'Hebrew',
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        delaySeconds:  60*5
-      })
-    })
+    // it('Error message for lexemes response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/lexemes',
+    //     language:'Hebrew',
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+    //     delaySeconds:  60*5
+    //   })
+    // })
   
-    it('Error message for lexemes response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in english mode',()=>{
-      cy.searchRequest({
-        url:'/lexemes',
-        language:'English',
-        message:'Oops. Something went wrong Please try again later',
-        delaySeconds: 60*5
-      })
-    })
+    // it('Error message for lexemes response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/lexemes',
+    //     language:'English',
+    //     message:'Oops. Something went wrong Please try again later',
+    //     delaySeconds: 60*5
+    //   })
+    // })
   
-    it('Error message for lexemes response with status code 500 when clicking the run button of search page'+
-    ' in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/lexemes',
-        language:'Hebrew',
-        status:500,
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-      })
-    })
+    // it('Error message for lexemes response with status code 500 when clicking the run button of search page'+
+    // ' in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/lexemes',
+    //     language:'Hebrew',
+    //     status:500,
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+    //   })
+    // })
   
-    it('Error message for lexemes response with status code 500 when clicking the run button of search page'+
-    ' in english mode',()=>{
-      cy.searchRequest({
-        url:'/lexemes',
-        language:'English',
-        status:500,
-        message:'Oops. Something went wrong Please try again later'
-      })
-    })  
+    // it('Error message for lexemes response with status code 500 when clicking the run button of search page'+
+    // ' in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/lexemes',
+    //     language:'English',
+    //     status:500,
+    //     message:'Oops. Something went wrong Please try again later'
+    //   })
+    // })  
      
   
     
-    it('Error message for wordforms response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/wordforms',
-        language:'Hebrew',
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-        delaySeconds:  60*5
-      })
-    })
+    // it('Error message for wordforms response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/wordforms',
+    //     language:'Hebrew',
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+    //     delaySeconds:  60*5
+    //   })
+    // })
   
-    it('Error message for wordforms response with a delay of 5 minutes when clicking the run button'+
-    ' of search page in english mode',()=>{
-      cy.searchRequest({
-        url:'/wordforms',
-        language:'English',
-        message:'Oops. Something went wrong Please try again later',
-        delaySeconds: 60*5
-      })
-    })
+    // it('Error message for wordforms response with a delay of 5 minutes when clicking the run button'+
+    // ' of search page in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/wordforms',
+    //     language:'English',
+    //     message:'Oops. Something went wrong Please try again later',
+    //     delaySeconds: 60*5
+    //   })
+    // })
   
-    it('Error message for wordforms response with status code 500 when clicking the run button of search page'+
-    ' in hebrew mode',()=>{
-      cy.searchRequest({
-        url:'/wordforms',
-        language:'Hebrew',
-        status:500,
-        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-      })
-    })
+    // it('Error message for wordforms response with status code 500 when clicking the run button of search page'+
+    // ' in hebrew mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/wordforms',
+    //     language:'Hebrew',
+    //     status:500,
+    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+    //   })
+    // })
   
-    it('Error message for wordforms response with status code 500 when clicking the run button of search page'+
-    ' in english mode',()=>{
-      cy.searchRequest({
-        url:'/wordforms',
-        language:'English',
-        status:500,
-        message:'Oops. Something went wrong Please try again later'
-      })
-    })  
+    // it('Error message for wordforms response with status code 500 when clicking the run button of search page'+
+    // ' in english mode',()=>{
+    //   cy.searchRequest({
+    //     url:'/wordforms',
+    //     language:'English',
+    //     status:500,
+    //     message:'Oops. Something went wrong Please try again later'
+    //   })
+    // })  
   
       
   
